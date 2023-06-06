84536bdd2334c1823a295ae16dc8c5d473f2e818
IbrahimSamir0 <ibrahim20208002@fci.helwan.edu.eg>
2023-06-06 15:16:28 +0300

    6 June, 2023

--- prescription/bot3/constant.py
+++
@@ -1,7 +0,0 @@
-
-NHI_URL = 'https://www.cancer.gov/about-cancer/treatment/drugs/lung'
-BASE_URL = "https://www.drugs.com/"
-interaction_URL = 'https://www.drugs.com/drug-interactions/onabotulinumtoxina,botox.html'
-
-# List of Cancer Chemotherapy Drugs
-drugs_URL= 'https://www.navigatingcare.com/library/all/chemotherapy_drugs'
